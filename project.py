89f0e4440287e91246882f2540f3ea77f0d0f7ba
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-06-25 10:42:00 +0100

    Add Discogs OAuth integration, update dependencies, and ignore cache file

--- project.py
+++ project.py
@@ -3,6 +3,7 @@ import sys
 import os
 import logging
 import requests
+from oauthlib.oauth2 import WebApplicationClient
 from requests.adapters import HTTPAdapter
 from requests_cache import CachedSession
 from urllib3.util.retry import Retry
@@ -19,7 +20,6 @@ logging.basicConfig(
     ]
 )
 logger = logging.getLogger(__name__)
-discogs_data = {}
 
 # Init Requests Cache
 session = CachedSession(
@@ -27,6 +27,10 @@ session = CachedSession(
     backend="sqlite",
     expire_after=1800
 )
+DISCOGS_DATA = {}
+DISCOGS_KEY = os.getenv("DISCOGS_KEY")
+DISCOGS_SECRET = os.getenv("DISCOGS_SECRET")
+DISCOGS_API_TOKEN = None
 
 # Initialize Rich's console
 console = Console()
@@ -37,6 +41,25 @@ app = typer.Typer(
     no_args_is_help=True,
     pretty_exceptions_short=False
 )
+
+# OAuth
+client = WebApplicationClient(str(DISCOGS_KEY))
+auth_url = "https://www.discogs.com/oauth/authorize"
+
+url = client.prepare_request_uri(
+    auth_url,
+    scope = "read:user",
+)
+data = client.prepare_request_body(
+    client_id=DISCOGS_KEY,
+    client_secret=DISCOGS_SECRET,
+)
+
+token_url = "https://api.discogs.com/oauth/request_token"
+res = requests.post(token_url, data=data)
+print(res)
+
+
 
 # Entry Point
 @app.callback(invoke_without_command=True)
@@ -54,6 +77,13 @@ def hello(name: str = "world"):
 @app.command()
 def get_albums():
     print("Getting Albums")
+
+# Discogs API
+def get_discogs_api_token():
+    logger.info("get_discogs_api_token(): Getting Discogs API Token")
+    global DISCOGS_API_TOKEN
+    
+
 
 if __name__ == "__main__":
     app()
